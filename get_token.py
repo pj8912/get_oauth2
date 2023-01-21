1da59c00ae60798c757f1140cc08872a5cc384af
jp <59218902+pj8912@users.noreply.github.com>
2023-01-21 14:23:43 +0530

    initial commit
    for blogger scope

---
+++ get_token.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlencode
+import webbrowser
+
+# Set the client ID and redirect URI
+client_id = "your_client_id"
+redirect_uri = "your_redirect_uri"
+
+# Set the scope of access
+scope = "https://www.googleapis.com/auth/blogger"
+
+# Build the authorization URL
+query_params = {
+    "response_type": "code",
+    "client_id": client_id,
+    "redirect_uri": redirect_uri,
+    "scope": scope
+}
+
+url = f"https://accounts.google.com/o/oauth2/auth?{urlencode(query_params)}"
+
+# Open the authorization URL in the user's web browser
+webbrowser.open(url)
